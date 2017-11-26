acc7f67cb952812dfa055871aa5db8346883c749
zima2219981998 <michal_zimka@o2.pl>
2017-11-26 22:55:11 +0100

    PATH(becouse I forgot to add files to the git -.- ) query of view user and his posts in repository (blog_user)

---
+++ src/Zima/BlogwebBundle/Repository/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Zima\BlogwebBundle\Repository;
+
+use Zima\BlogwebBundle\Entity\User;
+
+class UserRepository extends \Doctrine\ORM\EntityRepository
+{
+    /**
+     * @return array
+     * @param User $user
+     */
+    public function findInfo(User $user) {
+        return $this->createQueryBuilder("user")
+            ->where("user.id = :user")
+            ->setParameter("user", $user->getId())
+            ->getQuery()
+            ->getResult();
+    }
+}
